171f66b8a5af583d88e2d0958a0b8e194d4c6693
jacoyutorius <jacoyutorius@gmail.com>
2021-02-13 03:18:10 +0900

    apply favicon, header logo

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,10 @@
   <div id="app">
     <nav class="navbar is-dark" role="navigation" aria-label="main navigation">
       <div class="navbar-brand">
-        <a class="navbar-item" href="https://bulma.io">
-          <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
+        <a class="navbar-item" href="/">
+          <img src="/images/monologue.png" width="112" height="28">
         </a>
-
+        
         <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
